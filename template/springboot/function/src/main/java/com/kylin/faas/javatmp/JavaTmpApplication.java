8c3793c15867b3598eb7ca8abda74c8bbaa1926d
LeBW <27091925@qq.com>
2022-01-01 00:15:18 +0800

    add ServletComponentScan to make filter work

--- template/springboot/function/src/main/java/com/kylin/faas/javatmp/JavaTmpApplication.java
+++ template/springboot/function/src/main/java/com/kylin/faas/javatmp/JavaTmpApplication.java
@@ -2,11 +2,13 @@ package com.kylin.faas.javatmp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@ServletComponentScan
 public class JavaTmpApplication {
 
     public static void main(String[] args) {
